a54453d13ba97836cf35a38d7cc3ef3d34a49e60
MarlonVictor <cmarlonvictor11@gmail.com>
2025-02-19 22:53:57 -0300

    feat: home com componente SingleItem

---
+++ web/src/components/SingleItem.vue
@@ -0,0 +1,44 @@
+<script setup>
+import IconPlay from '@/components/Icons/IconPlay.vue'
+
+const { cover, title, subtitle } = defineProps({
+  cover: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+  },
+  subtitle: {
+    type: String,
+  },
+})
+</script>
+
+<template>
+  <li
+    class="flex flex-col gap-2 p-3 rounded-lg cursor-pointer hover:bg-white/5 transition-colors group"
+  >
+    <div class="relative">
+      <img :src="cover" class="rounded-lg size-40" />
+
+      <button
+        data-testid="play-button"
+        :aria-label="`Tocar ${title || subtitle}`"
+        class="absolute grid place-items-center bottom-2 right-2 z-10 size-12 bg-primary-500 rounded-full cursor-pointer shadow-play translate-y-2 opacity-0 hover:bg-primary-400 hover:scale-105 group-hover:translate-y-0 group-hover:opacity-100 transition-all duration-[400ms]"
+      >
+        <IconPlay class="size-6" />
+      </button>
+    </div>
+
+    <div class="flex flex-col gap-1 items-start">
+      <a href="#" class="text-white font-semibold line-clamp-2 w-40 hover:underline">
+        {{ title }}
+      </a>
+
+      <a href="#" class="text-neutral-400 font-medium line-clamp-2 text-sm w-40 hover:underline">
+        {{ subtitle }}
+      </a>
+    </div>
+  </li>
+</template>
--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -1,7 +1,71 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import HeaderComponent from '@/components/HeaderComponent.vue'
+import SingleItem from '@/components/SingleItem.vue'
 </script>
 
 <template>
-  <TheWelcome />
+  <main class="flex flex-col h-screen bg-black">
+    <HeaderComponent />
+
+    <div class="flex flex-1 px-2">
+      <section
+        class="relative flex-1 p-4 bg-neutral-900 rounded-lg overflow-hidden flex flex-col gap-6"
+      >
+        <span class="bg-[rgb(224,56,40)] h-80 block absolute top-0 left-0 right-0"></span>
+        <span
+          class="bg-[linear-gradient(0deg,rgba(31,31,31,1)10%,rgba(31,31,31,0.85)68%,rgba(31,31,31,0.6)100%)] h-80 block absolute top-0 left-0 right-0"
+        ></span>
+
+        <div>
+          <header class="relative flex items-end justify-between z-10 pb-1 px-3 h-12">
+            <h1 class="hover:underline text-white text-2xl font-bold cursor-pointer">
+              Artistas populares
+            </h1>
+
+            <p class="hover:underline text-neutral-400 text-sm font-bold cursor-pointer">
+              Mostrar tudo
+            </p>
+          </header>
+
+          <ul class="flex relative z-10">
+            <SingleItem
+              cover="https://i.scdn.co/image/ab6761610000517429af2ffb6f4ddd6324f878bc"
+              title="Korn"
+              subtitle="Artista"
+            />
+          </ul>
+        </div>
+
+        <div>
+          <header class="relative flex items-end justify-between z-10 pb-1 px-3 h-12">
+            <h1 class="hover:underline text-white text-2xl font-bold cursor-pointer">
+              Músicas populares
+            </h1>
+
+            <p class="hover:underline text-neutral-400 text-sm font-bold cursor-pointer">
+              Mostrar tudo
+            </p>
+          </header>
+
+          <ul class="flex relative z-10">
+            <SingleItem
+              cover="https://upload.wikimedia.org/wikipedia/pt/3/3a/Follow_the_Leader_-_Korn.png"
+              title="Freak On a Leash"
+              subtitle="Follow The Leader"
+            />
+            <SingleItem
+              cover="https://upload.wikimedia.org/wikipedia/pt/3/3a/Follow_the_Leader_-_Korn.png"
+              title="Got the Life"
+              subtitle="Follow The Leader"
+            />
+            <SingleItem
+              cover="https://upload.wikimedia.org/wikipedia/pt/f/f4/KornIssuesArt.jpeg"
+              title="Falling Away from Me"
+              subtitle="Issues"
+            />
+          </ul>
+        </div>
+      </section>
+    </div>
+  </main>
 </template>
